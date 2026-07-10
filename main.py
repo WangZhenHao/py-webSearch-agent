fe4b7ceea346d65933ea889bedbc0b5517185f13
Poet wang <zhenhaowang@dimleap.ai>
2026-07-10 14:44:34 +0800

    feat: 初始化

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
 import os
 
 load_dotenv()
 
+llm = init_chat_model(
+    model="gpt-5.4",
+    api_key=os.getenv("PY_OPENAI_API_KEY"),
+    base_url=os.getenv("PY_OPENAI_BEARER_URL")
+)
+
+
 def main():
-    print(f"Hello from py-websearch-agent! {os.getenv('PY_OPENAI_API_KEY')}")
+    # 调用模型
+    response = llm.invoke("你好，请介绍一下自己,是什么模型")
+    print(response.content)
 
 
 if __name__ == "__main__":
